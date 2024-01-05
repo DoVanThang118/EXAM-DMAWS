f7af4de500da223cfb05a0193191d6d7c9f19cbe
WinHo <dovanthang118@gmail.com>
2024-01-06 01:52:45 +0700

    Add project files.

---
+++ EXAM-DMAWS/Entities/ExamNetContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EXAM_DMAWS.Entities;
+
+public partial class ExamNetContext : DbContext
+{
+    public ExamNetContext()
+    {
+    }
+
+    public ExamNetContext(DbContextOptions<ExamNetContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-6N5JNCN;Initial Catalog=exam.net;Integrated Security=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
